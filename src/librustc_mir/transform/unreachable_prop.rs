e755a17c2aa341512474c61087a060f8dac3bc9c
Konstantinos Triantafyllou <ko.trian@gmail.com>
2019-12-06 22:01:52 +0100

    Improve unreachable propagation mir optimization pass

--- src/librustc_mir/transform/unreachable_prop.rs
+++ src/librustc_mir/transform/unreachable_prop.rs
@@ -4,34 +4,94 @@
 
 use rustc::ty::TyCtxt;
 use rustc::mir::*;
-use rustc_data_structures::fx::FxHashSet;
+use rustc_data_structures::fx::{FxHashSet, FxHashMap};
+use std::borrow::Cow;
 use crate::transform::{MirPass, MirSource};
+use crate::transform::simplify;
 
 pub struct UnreachablePropagation;
 
 impl MirPass<'_> for UnreachablePropagation {
-    fn run_pass<'tcx>(&self, _tcx: TyCtxt<'tcx>, _: MirSource<'tcx>, body: &mut Body<'tcx>) {
+    fn run_pass<'tcx>(&self, _tcx: TyCtxt<'tcx>, _: MirSource<'tcx>, body: &mut BodyCache<'tcx>) {
         let mut unreachable_blocks = FxHashSet::default();
+        let mut replacements = FxHashMap::default();
 
         for (bb, bb_data) in traversal::postorder(body) {
-            if let Some(terminator) = &bb_data.terminator {
-                if let TerminatorKind::Unreachable = terminator.kind {
-                    unreachable_blocks.insert(bb.clone());
-                } else {
-                    let mut bb_successors = terminator.successors().peekable();
+            let terminator = bb_data.terminator();
 
-                    if bb_successors.peek().is_some() {
-                        if bb_successors.all(|bb| unreachable_blocks.contains(bb)) {
-                            unreachable_blocks.insert(bb.clone());
-                        }
+            if let TerminatorKind::Unreachable = terminator.kind {
+                unreachable_blocks.insert(bb);
+            } else {
+                let is_unreachable_pred = |succ: &'_ BasicBlock| unreachable_blocks.contains(succ);
+                let mut mut_terminator = terminator.clone();
+
+                if remove_successors(&mut mut_terminator, is_unreachable_pred) {
+                    replacements.insert(bb, mut_terminator.kind.clone());
+                    if let TerminatorKind::Unreachable = mut_terminator.kind {
+                        unreachable_blocks.insert(bb);
                     }
                 }
             }
         }
 
-        for block in unreachable_blocks {
-            body.basic_blocks_mut()[block]
-                .terminator.as_mut().unwrap().kind = TerminatorKind::Unreachable;
+        let replaced = !replacements.is_empty();
+        for (bb, terminator_kind) in replacements {
+            body.basic_blocks_mut()[bb].terminator_mut().kind = terminator_kind;
+        }
+
+        if replaced {
+            simplify::remove_dead_blocks(body);
         }
     }
+}
+
+fn remove_successors<F>(terminator: &mut Terminator<'_>, pred: F) -> bool
+    where F: Fn(&BasicBlock) -> bool
+{
+    let mut removed_successor = false;
+
+    match &terminator.kind {
+        TerminatorKind::Goto { target } => {
+            if pred(target) {
+                terminator.kind = TerminatorKind::Unreachable;
+                removed_successor = true;
+            }
+        },
+        TerminatorKind::SwitchInt { discr, switch_ty, values, targets } => {
+            let original_targets_len = targets.len();
+            let (otherwise, targets) = targets.split_last().unwrap();
+            let retained = values.iter().zip(targets.iter()).filter(|(_, t)| !pred(t)).
+                collect::<Vec<_>>();
+            let mut values = retained.iter().map(|&(v, _)| *v).collect::<Vec<_>>();
+            let mut targets = retained.iter().map(|&(_, d)| *d).collect::<Vec<_>>();
+
+            if !pred(otherwise) {
+                targets.push(*otherwise);
+            }
+
+            let retained_targets_len = targets.len();
+
+            if targets.is_empty() {
+                terminator.kind = TerminatorKind::Unreachable;
+            } else if targets.len() == 1 {
+                terminator.kind = TerminatorKind::Goto { target: targets[0] }
+            } else {
+                if values.len() == retained_targets_len {
+                    values.truncate(retained_targets_len - 1);
+                }
+
+                terminator.kind = TerminatorKind::SwitchInt {
+                    discr: discr.clone(),
+                    switch_ty,
+                    values: Cow::from(values),
+                    targets
+                }
+            }
+
+            removed_successor = original_targets_len != retained_targets_len;
+        },
+        _ => {}
+    }
+
+    removed_successor
 }
