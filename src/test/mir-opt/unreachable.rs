d6d6507163737b4b0743c9d354e2f176f41596a5
Konstantinos Triantafyllou <ko.trian@gmail.com>
2019-12-20 09:04:44 +0100

    Adjust failing mir-opt testcases

--- src/test/mir-opt/unreachable.rs
+++ src/test/mir-opt/unreachable.rs
@@ -20,26 +20,26 @@ fn main() {
 
 // END RUST SOURCE
 // START rustc.main.UnreachablePropagation.before.mir
-//      bb5: {
-//          ...
-//          switchInt(_6) -> [false: bb7, otherwise: bb6];
-//      }
-//      bb6: {
-//          ...
-//      }
 //      bb7: {
 //          ...
+//          goto -> bb9;
 //      }
 //      bb8: {
 //          ...
+//          goto -> bb9;
 //      }
 //      bb9: {
 //          ...
+//          unreachable;
 //      }
 //  }
 // END rustc.main.UnreachablePropagation.before.mir
 // START rustc.main.UnreachablePropagation.after.mir
-//      bb5: {
+//      bb7: {
+//          ...
+//          unreachable;
+//      }
+//      bb8: {
 //          ...
 //          unreachable;
 //      }
